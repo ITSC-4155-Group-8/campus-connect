42d115a7124c8859ebbce8962aeed95ba7f87178
Noah Piraino <noahapiraino@gmail.com>
2024-03-11 15:01:08 -0400

    chakra style footer

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,19 +1,30 @@
-import { Link } from "wouter";
-import { Box, Container, Flex } from "@chakra-ui/react";
+import { Link } from 'wouter'
+import {
+    Box,
+    Container,
+    Stack,
+    Text,
+    useColorModeValue,
+} from '@chakra-ui/react'
 
-function Footer() {
-	return <>
-		<Box as="footer" className="footer" paddingY="2rem" h="125">
-			<Container as="nav" maxWidth="container.xl">
-				<Flex align="center" justify="center">
-					<Flex gap="1rem" justify="center">
-						<Link href="/">Contact Us</Link>
-						<Link href="/">About</Link>
-					</Flex>
-				</Flex>
-			</Container>
-		</Box>
-	</>
+export default function Footer() {
+    return (
+        <Box
+            bg={useColorModeValue('gray.50', 'gray.900')}
+            color={useColorModeValue('gray.700', 'gray.200')}>
+            <Container
+                as={Stack}
+                maxW={'6xl'}
+                py={4}
+                direction={{ base: 'column', md: 'row' }}
+                spacing={4}
+                justify={{ base: 'center', md: 'space-between' }}
+                align={{ base: 'center', md: 'center' }}>
+                <Text>© 2024 CampusConnect. All rights reserved</Text>
+                <Stack direction={'row'} spacing={6}>
+                    <Link href="/about">About Us</Link>
+                </Stack>
+            </Container>
+        </Box>
+    )
 }
-
-export default Footer;
