1697f65ff98bec80947212be7a223ac60b74cd66
Noah Piraino <noahapiraino@gmail.com>
2024-03-11 14:57:47 -0400

    chakra style header

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,21 +1,160 @@
 import { Link } from "wouter";
-import { Box, Container, Heading, Flex } from "@chakra-ui/react";
+import {
+    Box,
+    Flex,
+    Text,
+    IconButton,
+    Button,
+    Stack,
+    Collapse,
+    Popover,
+    PopoverTrigger,
+    useColorModeValue,
+    useBreakpointValue,
+    useDisclosure,
+} from '@chakra-ui/react'
+import { HamburgerIcon } from '@chakra-ui/icons'
 
-function Header() {
-	return <>
-		<Box as="header" className="header" paddingY="0.3rem">
-			<Container as="nav" maxWidth="container.xl">
-				<Flex align="center" justify="space-between">
-					<Heading pb="0.5rem">Campus Connect</Heading>
-					<Flex gap="1rem" justify="center">
-						<Link href="/">Home</Link>
-						<Link href="/login">Login</Link>
-						<Link href="/apitest">API Test Page</Link>
-					</Flex>
-				</Flex>
-			</Container>
-		</Box>
-	</>
+export default function WithSubnavigation() {
+    const { isOpen, onToggle } = useDisclosure()
+
+    return (
+        <Box>
+            <Flex
+                bg={useColorModeValue('white', 'gray.800')}
+                color={useColorModeValue('gray.600', 'white')}
+                minH={'60px'}
+                py={{ base: 2 }}
+                px={{ base: 4 }}
+                borderBottom={1}
+                borderStyle={'solid'}
+                borderColor={useColorModeValue('gray.200', 'gray.900')}
+                align={'center'}>
+                <Flex
+                    flex={{ base: 1, md: 'auto' }}
+                    ml={{ base: -2 }}
+                    display={{ base: 'flex', md: 'none' }}>
+                    <IconButton
+                        onClick={onToggle}
+                        icon={<HamburgerIcon />}
+                        variant={'ghost'}
+                        aria-label={'Toggle Navigation'}
+                    />
+                </Flex>
+                <Flex flex={{ base: 1 }} justify={{ base: 'center', md: 'start' }}>
+                    <Text
+                        textAlign={useBreakpointValue({ base: 'center', md: 'left' })}
+                        fontFamily={'heading'}
+                        color={useColorModeValue('gray.800', 'white')}>
+                        CampusConnect
+                    </Text>
+
+                    <Flex display={{ base: 'none', md: 'flex' }} ml={10}>
+                        <DesktopNav />
+                    </Flex>
+                </Flex>
+
+                <Stack
+                    flex={{ base: 1, md: 0 }}
+                    justify={'flex-end'}
+                    direction={'row'}
+                    spacing={6}>
+                    <Button as={'a'} fontSize={'sm'} fontWeight={400} variant={'link'} href={'#'}>
+                        Sign In
+                    </Button>
+                    <Button
+                        as={'a'}
+                        display={{ base: 'none', md: 'inline-flex' }}
+                        fontSize={'sm'}
+                        fontWeight={600}
+                        color={'white'}
+                        bg={'pink.400'}
+                        href={'#'}
+                        _hover={{
+                            bg: 'pink.300',
+                        }}>
+                        Sign Up
+                    </Button>
+                </Stack>
+            </Flex>
+
+            <Collapse in={isOpen} animateOpacity>
+                <MobileNav />
+            </Collapse>
+        </Box>
+    )
 }
 
-export default Header;
+const DesktopNav = () => {
+    const linkColor = useColorModeValue('gray.600', 'gray.200')
+    const linkHoverColor = useColorModeValue('gray.800', 'white')
+
+    return (
+        <Stack direction={'row'} spacing={4}>
+            {NAV_ITEMS.map((navItem) => (
+                <Box key={navItem.label}>
+                    <Box
+                        as={Link}
+                        p={2}
+                        href={navItem.href ?? '#'}
+                        fontSize={'sm'}
+                        fontWeight={500}
+                        color={linkColor}
+                        _hover={{
+                            textDecoration: 'none',
+                            color: linkHoverColor,
+                        }}>
+                        {navItem.label}
+                    </Box>
+                </Box>
+            ))}
+        </Stack>
+    )
+}
+
+const MobileNav = () => {
+    return (
+        <Stack bg={useColorModeValue('white', 'gray.800')} p={4} display={{ md: 'none' }}>
+            {NAV_ITEMS.map((navItem) => (
+                <MobileNavItem key={navItem.label} {...navItem} />
+            ))}
+        </Stack>
+    )
+}
+
+const MobileNavItem = ({ label, href }: NavItem) => {
+    return (
+        <Stack spacing={4}>
+            <Box
+                py={2}
+                as={Link}
+                href={href ?? '#'}
+                justifyContent="space-between"
+                alignItems="center"
+                _hover={{
+                    textDecoration: 'none',
+                }}>
+                <Text fontWeight={600} color={useColorModeValue('gray.600', 'gray.200')}>
+                    {label}
+                </Text>
+            </Box>
+        </Stack>
+    )
+}
+
+interface NavItem {
+    label: string
+    subLabel?: string
+    href?: string
+}
+
+const NAV_ITEMS: Array<NavItem> = [
+    {
+        label: 'Home',
+        href: '/',
+    },
+    {
+        label: 'API Test',
+        href: "/apitest",
+    }
+]
