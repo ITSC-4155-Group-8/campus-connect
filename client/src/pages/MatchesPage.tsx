04a089a16b1174e4ef044e1785a1ebc9bcead793
ajbertlshofer <abertlsh@uncc.edu>
2024-04-22 17:28:26 -0400

    Updated matched to loop and space cards

--- client/src/pages/MatchesPage.tsx
+++ client/src/pages/MatchesPage.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import { 
     Input,
     Flex,
@@ -13,26 +15,46 @@ import {
  } from '@chakra-ui/react'
  import UserCard from '../components/UserCard';
 function MatchesPage() {
+    const [ state, setState ] = useState([
+        {
+            first_name: "Andrew",
+            last_name: "Bertlshofer",
+            year: "Senior",
+            major: "CS",
+            email: "abertlsh@uncc.edu"
+        },
+        {
+            first_name: "Andrew",
+            last_name: "Bertlshofer",
+            year: "Senior",
+            major: "CS",
+            email: "abertlsh@uncc.edu"
+        },
+        {
+            first_name: "Andrew",
+            last_name: "Bertlshofer",
+            year: "Senior",
+            major: "CS",
+            email: "abertlsh@uncc.edu"
+        }
+    ])
     return (
         <>
             <Flex
             justifyContent={'flex-start'}
             padding='10'
-            direction={'column'}>
+            direction={'column'}
+            gap={"1rem"}>
                 <Text
                 fontSize='30px'>
                     Users You Matched With:
                 </Text>  
+                {
+                state.map(user => <UserCard user={user}/>)
+            }
             </Flex>
-
-            <UserCard user={{
-                first_name: "Andrew",
-                last_name: "Bertlshofer",
-                year: "Senior",
-                major: "CS",
-                email: "abertlsh@uncc.edu"
-            }}/>
-
+           
+            
         </>
     )
 }
