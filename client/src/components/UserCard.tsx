4a6f63c1a17e429967a4afeec781d7546e53b70f
ajbertlshofer <abertlsh@uncc.edu>
2024-04-25 12:04:36 -0400

    added border

--- client/src/components/UserCard.tsx
+++ client/src/components/UserCard.tsx
@@ -4,7 +4,7 @@ export default function UserCard(props) {
     const user = props.user;
     return (
             <Card
-            backgroundColor={useColorModeValue('blue.200', 'gray.800')}>
+            backgroundColor={useColorModeValue('blue.200', 'gray.800')}style={{ border: "2px solid black" }}>
                 <CardHeader>
                     <Heading size='md'>{user.first_name} {user.last_name}</Heading>
                 </CardHeader>
