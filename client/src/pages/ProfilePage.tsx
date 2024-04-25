cc44c93c45f7515735af60eb285c30a7b9c58876
Pedro <pqs2000@yahoo.com>
2024-04-25 14:24:56 -0400

    Started working on api call

--- client/src/pages/ProfilePage.tsx
+++ client/src/pages/ProfilePage.tsx
@@ -1,45 +1,73 @@
-// import { Link } from "react-router-dom";
-import { Container, Box, Text, Flex, Image, Heading } from '@chakra-ui/react'
+//import { Link } from "react-router-dom";
+import { Container, Box, Text, Flex, Image, Heading, useRadio } from '@chakra-ui/react'
 import imgsrc from "../assets/profilePic.jpg"
+import { useState, useEffect } from "react";
 
-function ProfilePage() {
+interface User {
+    name: string;
+    age: number;
+    major: string;
+    bio: string;
+    // Add other properties as needed
+}
+
+function ProfilePage()  {
+    
+    const [state, setState] = useState<{
+        loading: boolean;
+        user: User | null;
+        loggedin: boolean;
+    }>({
+        loading: true,
+        user: null,
+        loggedin: false
+    });
+
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const response = await fetch(apiURL + '/profile', {
+                    method: 'GET', 
+                    credentials: "include",
+                })
+                if (response.status == 404) {
+                    setState({ user: null, loading: false, loggedin: true })
+                } else {
+                    const json = await response.json()
+                    setState({ user: json, loading: false, loggedin: true })
+                }
+            } catch {
+                setState({ user: null, loading: false, loggedin: false })
+            }
+        }
+
+        fetchData();
+    }, [])
+    
+    {state.user && (
+    console.log(state.user.name)
+    )}
+
     return (
         <>
-            <Flex
-            justifyContent={'flex-start'}
-            padding='10'
-            direction={'row'}
-            gap={"50px"}>
-                <Image src={imgsrc} height='100px' width='100px' padding={"0"} />
-
-
-                <Heading
-                fontSize='30px'>
-                    John Smith
-                </Heading>  
-
+            <Flex justifyContent='flex-start' padding='10' direction='row' gap={4}>
+                <Image src={imgsrc} height='100px' width='100px' padding='0' />
+                <Flex justifyContent='center' alignItems='center'>
+                {state.user && (
+                        <Heading fontSize='30px'>{state.user.name}</Heading>
+                    )}
+                </Flex>
             </Flex>
 
-            <Flex
-            gap={"15"}
-            direction={"column"}>
-            <Text>
-                Age: 
-            </Text>
-            <Text>
-                Hometown: 
-            </Text>
-            <Text>
-                Major: 
-            </Text>
-            <Text>
-                Bio: 
-                
-            </Text>
+            {state.user && (
+            <Flex gap={4} direction='column'>
+                <Text>Age: {state.user.age}</Text>
+                <Text>Major: {state.user.major}</Text>
+                <Text>Bio: {state.user.bio}</Text>
             </Flex>
-
+            )}
         </>
-    )
+    );
 }
 
 export default ProfilePage;
