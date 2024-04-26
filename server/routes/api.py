0fd25414d4fc45e17677987f7c1ecb7d30cae6d0
Noah Piraino <noahapiraino@gmail.com>
2024-04-25 20:21:19 -0400

    fix endpoints

--- server/routes/api.py
+++ server/routes/api.py
@@ -3,6 +3,8 @@ import datetime
 from flask import Blueprint, jsonify, request, Response
 from flask_login import current_user, login_required
 
+from bson import ObjectId
+
 from src.db import db, login_collection, user_data_collection, match_collection
 from src.user import User
 from src.server_functions import create_record
@@ -57,7 +59,12 @@ def get_matches():
     user_data = user_data_collection.find_one({'email': User.get_user_by_id(current_user.id).__dict__['email']})
     if not user_data:
         return Response(status=500)
-    return jsonify(user_data['match_queue'])
+    match_arr = []
+    for m in user_data['match_queue']:
+        match_data = match_collection.find_one({'_id': ObjectId(m)})
+        match_data.pop('_id')
+        match_arr.append(match_data)
+    return jsonify(match_arr)
 
 
 @api_routes.route("/api/matches/<match_id>", methods = [ 'GET' ])
@@ -70,18 +77,19 @@ def get_match_data(match_id=None):
     if match_id not in user_data['match_queue']:
         return Response(status=404)
     
-    match = match_collection.find_one(match_id)
+    match = match_collection.find_one({'_id': ObjectId(match_id)})
     if not match:
         return Response(status=404)
-
+    match.pop('_id')
     return jsonify(match)
 
 
 @api_routes.route("/api/users/<user_id>", methods = [ 'GET' ])
 @login_required
 def get_user_data(user_id=None):
-    user_data = user_data_collection.find_one(user_id)
+    print(user_id)
+    user_data = user_data_collection.find_one({'_id': ObjectId(user_id)})
     if not user_data:
         return Response(status=404)
-    
+    user_data.pop('_id')
     return jsonify(user_data)
