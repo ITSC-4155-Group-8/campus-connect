500af007db13bd968d0e5313de7cf02a62deb88a
Noah Piraino <noahapiraino@gmail.com>
2024-04-29 12:49:47 -0400

    show pending if they haven't matched yet

--- client/src/components/UserCard.tsx
+++ client/src/components/UserCard.tsx
@@ -4,79 +4,79 @@ import { useState } from 'react';
 export default function UserCard(props) {
     const { user, match, owner } = props;
     const [buttonText, setButtonText] = useState((owner ? match.owner_wants_match : match.match_wants_match) ? "True" : "False");
-    const handleClick =async () => {setButtonText('True')
-        const response = await fetch(apiURL + '/matches/'+match.match_object_id+"/accept", {
+    const handleClick = async () => {
+        setButtonText('True')
+        const response = await fetch(apiURL + '/matches/' + match.match_object_id + "/accept", {
             credentials: "include",
             method: "POST",
         })
-    } ;
+    };
     return (
-            <Card
-            backgroundColor={useColorModeValue('blue.200', 'gray.800')}style={{ border: "2px solid black" }}>
-                <CardHeader>
-                    <Heading size='md'>{user.first_name} {user.last_name}</Heading>
-                </CardHeader>
-                <CardBody>
-                    <Stack divider={<StackDivider />} spacing='2'>
-                        <Box>
-                            <Heading size='xs' textTransform='uppercase'>
-                                School Year
-                            </Heading>
-                            <Text pt='2' fontSize='sm'>
-                                {user.school_year}
-                            </Text>
-                        </Box>
-                        <Box>
-                            <Heading size='xs' textTransform='uppercase'>
-                                Major
-                            </Heading>
-                            <Text pt='2' fontSize='sm'>
-                                {user.major}
-                            </Text>
-                        </Box>
-                        <Box>
-                            <Heading size='xs' textTransform='uppercase'>
-                                Email
-                            </Heading>
-                            <Text pt='2' fontSize='sm'>
-                                {user.email}
-                            </Text>
-                        </Box>
-                        <Box>
-                            <Heading size='xs' textTransform='uppercase'>
-                                Compatibility Score
-                            </Heading>
-                            <Text pt='2' fontSize='sm'>
-                                {match.compatibility_score}
-                            </Text>
-                        </Box>
-                        <Box>
-                            <Heading size='xs' textTransform='uppercase'>
-                                Compatibility description
-                            </Heading>
-                            <Text pt='2' fontSize='sm'>
-                                {match.compatibility_description}
-                            </Text>
-                        </Box>
+        <Card
+            backgroundColor={useColorModeValue('blue.200', 'gray.800')} style={{ border: "2px solid black" }}>
+            <CardHeader>
+                <Heading size='md'>{user.first_name} {user.last_name}</Heading>
+            </CardHeader>
+            <CardBody>
+                <Stack divider={<StackDivider />} spacing='2'>
+                    <Box>
+                        <Heading size='xs' textTransform='uppercase'>
+                            School Year
+                        </Heading>
+                        <Text pt='2' fontSize='sm'>
+                            {user.school_year}
+                        </Text>
+                    </Box>
+                    <Box>
+                        <Heading size='xs' textTransform='uppercase'>
+                            Major
+                        </Heading>
+                        <Text pt='2' fontSize='sm'>
+                            {user.major}
+                        </Text>
+                    </Box>
+                    <Box>
+                        <Heading size='xs' textTransform='uppercase'>
+                            Email
+                        </Heading>
+                        <Text pt='2' fontSize='sm'>
+                            {user.email}
+                        </Text>
+                    </Box>
+                    <Box>
+                        <Heading size='xs' textTransform='uppercase'>
+                            Compatibility Score
+                        </Heading>
+                        <Text pt='2' fontSize='sm'>
+                            {match.compatibility_score}
+                        </Text>
+                    </Box>
+                    <Box>
+                        <Heading size='xs' textTransform='uppercase'>
+                            Compatibility description
+                        </Heading>
+                        <Text pt='2' fontSize='sm'>
+                            {match.compatibility_description}
+                        </Text>
+                    </Box>
+                    {(owner ? !match.owner_wants_match : !match.match_wants_match) ?
                         <Box>
                             <Heading size='xs' textTransform='uppercase'>
                                 Do you want to match?
                             </Heading>
                             <Button onClick={handleClick}>{buttonText}</Button>
                         </Box>
-                        
+                        :
                         <Box>
-                            <Heading size='xs' textTransform='uppercase'>
-                                Do they want to match?
+                            <Heading size='xs'>
+                                Waiting to match...
                             </Heading>
-                            <Text pt='2' fontSize='sm'>
-                                {(owner ? match.match_wants_match : match.owner_wants_match) ? "True" : "False"}
-                            </Text>
                         </Box>
-                    </Stack>
-                </CardBody>
-            </Card>
-        
+                    }
+                </Stack>
+            </CardBody>
+        </Card>
+
     )
-    
+
 }
